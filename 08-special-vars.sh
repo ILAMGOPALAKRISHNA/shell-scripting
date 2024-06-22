dd6163001b0d5dee869ae30685890603133d2877
ILAMGOPALAKRISHNA <ilamgopalakrishna111@gmail.com>
2024-06-22 15:31:50 +0530

    1st commit

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -9,6 +9,7 @@ echo "Current working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "Which user is running the script: $USER"
 echo "Hostname: $HOSTNAME"
+echo "exit status of previous command: $?"
 echo "process ID of the current shell script: $$"
 sleep 60 &
 echo "process ID of last background command: $!"
