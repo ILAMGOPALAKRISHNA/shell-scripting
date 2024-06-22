070f20c8db40ebcf74548c0b1ea07a7a0f1bf41b
ILAMGOPALAKRISHNA <ilamgopalakrishna111@gmail.com>
2024-06-22 11:57:55 +0530

    1st commit

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hello suresh, how are you?"
+echo "$PERSON2:: Hi $PERSON1, iam fine. How are you?"
+echo "$PERSON1:: I am fine too.how is your work?"
+echo "$PERSON2:: not bad. i am thinking to upgrade to devops. "
