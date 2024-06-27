564cdb95513a2006c0a31611286c7da4ddad469e
ILAMGOPALAKRISHNA <ilamgopalakrishna111@gmail.com>
2024-06-27 12:44:01 +0530

    1st commit

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SOURCE_DIRECTORY=/tmp/app-log
+SOURCE_DIRECTORY=/tmp/app-logs
 
 R="\e[31m"
 G="\e[32m"
@@ -13,3 +13,7 @@ else
   echo -e "$R please make sure $SOURCE_DIRECTORY exists $N"
   exit 1
 fi
+
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+
+echo "files to delete: $FILES"
